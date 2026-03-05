6416d717b6702cc70ed2f4899b604186234a7d5c
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 16:35:03 +0700

    feat: add new questions

--- src/client/routes/question.ts
+++ src/client/routes/question.ts
@@ -2,6 +2,11 @@ import express from "express";
 import { checkAuthentication } from "../middlewares/authentication";
 const router = express.Router();
 
+interface AddQuestionResponseInterface {
+	success: boolean;
+	questionID: string;
+}
+
 router.get("/questions/:id", checkAuthentication, async (req, res) => {
 	const fetchResponse = await fetch(
 		process.env.API_BASE_URL + `/api/questions/${req.params.id}`,
@@ -11,6 +16,35 @@ router.get("/questions/:id", checkAuthentication, async (req, res) => {
 		data: data,
 		authenticationData: req.authentication,
 	});
+});
+
+router.post("/question", checkAuthentication, async (req, res) => {
+	if (!req.authentication.loggedIn) {
+		res.redirect("/");
+	}
+
+	const addQuestionFetchResponse = await fetch(
+		process.env.API_BASE_URL + `/api/questions`,
+		{
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				content: req.body.content,
+				title: req.body.title,
+				username: req.authentication.username,
+				token: req.cookies["token"],
+			}),
+		},
+	);
+
+	const addQuestionData =
+		(await addQuestionFetchResponse.json()) as AddQuestionResponseInterface;
+
+	if (!addQuestionData.success) {
+		res.redirect("/");
+	}
+
+	res.redirect(`/questions/${addQuestionData.questionID}`);
 });
 
 export { router };
