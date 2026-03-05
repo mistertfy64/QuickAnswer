912c37c4826d071d347d96ead3d6f0404611769d
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 12:36:26 +0700

    feat: add logout route

--- src/server/routes/authentication.ts
+++ src/server/routes/authentication.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/authentication";
+import { register, login, logout } from "../controllers/authentication";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.get("/login", login);
+router.get("/logout", logout);
 
 module.exports = router;
