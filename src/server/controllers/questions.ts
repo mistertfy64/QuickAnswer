758669410510359a11ae841ce2c6e4099e82b516
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 11:48:27 +0700

    feat: add question controller for creating and getting questions

---
+++ src/server/controllers/questions.ts
@@ -0,0 +1,50 @@
+import { Question } from "../models/Question";
+import { Request, Response, NextFunction } from "express";
+import log from "../utilities/log";
+
+const addQuestion = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { title, content } = req.body;
+
+		if (!title || !content) {
+			res.status(400).json({ success: false });
+		}
+
+		const question = await Question.create({
+			title: title,
+			content: content,
+			askedBy: req.authentication.username,
+		});
+
+		res.status(200).json({
+			questionID: question._id,
+		});
+	} catch (error: unknown) {
+		res.status(400).json({ success: false });
+		if (error instanceof Error) {
+			log.error(error.stack);
+		}
+	}
+};
+
+const getQuestion = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const question = await Question.findById(req.params.id);
+
+		if (!question) {
+			res.status(404).json({ success: false });
+		}
+
+		res.status(200).json({
+			success: true,
+			question: question,
+		});
+	} catch (error: unknown) {
+		res.status(400).json({ success: false });
+		if (error instanceof Error) {
+			log.error(error.stack);
+		}
+	}
+};
+
+export { addQuestion, getQuestion };
