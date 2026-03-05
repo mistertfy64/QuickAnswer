202a73c894a9f6bf94c0cc9768739972ff7a27bc
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 21:21:57 +0700

    feat: add new PUT /question/:id route

--- src/server/routes/questions.ts
+++ src/server/routes/questions.ts
@@ -3,6 +3,7 @@ import {
 	addQuestion,
 	getQuestion,
 	getQuestions,
+	markQuestionAsSolved,
 } from "../controllers/questions";
 import { restrictToAuthenticated } from "../middlewares/authentication";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", getQuestions);
 router.get("/:id", getQuestion);
 router.post("/", restrictToAuthenticated, addQuestion);
+router.put("/:id", restrictToAuthenticated, markQuestionAsSolved);
 
 module.exports = router;
