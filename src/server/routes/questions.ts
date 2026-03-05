ac825c07a0618ff9f0d22bc407afdaf7ddaae53a
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 13:47:20 +0700

    feat: add `questions` route

--- src/server/routes/questions.ts
+++ src/server/routes/questions.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { addQuestion, getQuestion } from "../controllers/questions";
+import {
+	addQuestion,
+	getQuestion,
+	getQuestions,
+} from "../controllers/questions";
 import { restrictToAuthenticated } from "../middlewares/authentication";
 
 const router = express.Router();
 
+router.get("/", getQuestions);
 router.get("/:id", getQuestion);
 router.post("/", restrictToAuthenticated, addQuestion);
 
