35548c8b82b4c97b6f1608b7d62666034eb365d4
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 10:47:00 +0700

    feat: add authentication controller

---
+++ src/server/controllers/authentication.ts
@@ -0,0 +1,58 @@
+import { User } from "../models/User";
+import { Request, Response, NextFunction } from "express";
+import log from "../utilities/log";
+import { randomBytes } from "crypto";
+
+const register = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.create({
+			username,
+			password,
+		});
+		log.info(`Added new user with username ${user.username}.`);
+		res.status(200).json({ success: true });
+	} catch (error: unknown) {
+		res.status(400).json({ success: false });
+		if (error instanceof Error) {
+			log.error(error.stack);
+		}
+	}
+};
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+	const { username, password } = req.body;
+
+	if (!username || !password) {
+		return res.status(400).json({
+			success: false,
+			msg: "Email or password missing.",
+		});
+	}
+
+	const user = await User.findOne({ email: username }).select("+password");
+
+	if (!user) {
+		return res.status(400).json({ success: false, msg: "Invalid credentials" });
+	}
+
+	const correctPassword = await user.checkPassword(password);
+
+	if (!correctPassword) {
+		return res.status(400).json({ success: false, msg: "Invalid credentials" });
+	}
+
+	const token = randomBytes(24).toString("hex");
+	await user.addToken(token);
+
+	sendTokenResponse(res, token);
+};
+
+function sendTokenResponse(res: Response, token: string) {
+	res.cookie("token", token, {
+		sameSite: "lax",
+	});
+	res.status(200).json({ success: true });
+}
+
+export { register, login };
