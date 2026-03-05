2a2998ba9c109564d3cb9ac0a71ef99e951a0add
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 21:23:14 +0700

    feat: homepage now shows between solved and unsolved questions

--- src/client/routes/index.ts
+++ src/client/routes/index.ts
@@ -2,11 +2,30 @@ import express from "express";
 import { checkAuthentication } from "../middlewares/authentication";
 const router = express.Router();
 
+interface QuestionResponseInterface {
+	success: boolean;
+	questions: Array<{ [key: string]: unknown }>;
+}
+
 router.get("/", checkAuthentication, async (req, res) => {
-	const fetchResponse = await fetch(
-		process.env.API_BASE_URL + `/api/questions`,
+	const unsolvedQuestionResponse = await fetch(
+		process.env.API_BASE_URL + `/api/questions?solved=0`,
 	);
-	const data = await fetchResponse.json();
+
+	const unsolvedQuestions =
+		(await unsolvedQuestionResponse.json()) as QuestionResponseInterface;
+
+	const solvedQuestionResponse = await fetch(
+		process.env.API_BASE_URL + `/api/questions?solved=1`,
+	);
+
+	const solvedQuestions =
+		(await solvedQuestionResponse.json()) as QuestionResponseInterface;
+
+	const data = {
+		unsolvedQuestions: unsolvedQuestions.questions,
+		solvedQuestions: solvedQuestions.questions,
+	};
 
 	res.render("pages/index.ejs", {
 		data: data,
