876273bcc763c34296b0bfb9dde4fbe0c2ffc149
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-05 12:57:14 +0700

    fix: fix PORT env var to BACKEND_PORT

--- src/server/index.ts
+++ src/server/index.ts
@@ -37,7 +37,7 @@ app.use(`/api/authentication`, authentication);
 app.use(`/api/questions`, questions);
 app.use(`/api/answers`, answers);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.BACKEND_PORT || 5000;
 
 const server = app.listen(
 	PORT,
